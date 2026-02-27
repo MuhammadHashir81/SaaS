f89528b52a4a9ee236acacb8bf54939ff099fd8d
MuhammadHashir81 <MuhammadHashir8181@gmail.com>
2026-02-27 21:35:30 +0500

    working on admin dashboard

--- frontend/src/pages/admin/Dashboard.jsx
+++ frontend/src/pages/admin/Dashboard.jsx
@@ -23,6 +23,9 @@ const { RangePicker } = DatePicker;
 
 const Dashboard = () => {
 
+
+  // Cash flow trend data
+
   const data = [
     { month: 'Jan', inflow: 4000, outflow: 1200 },
     { month: 'Feb', inflow: 3000, outflow: 900 },
@@ -33,7 +36,105 @@ const Dashboard = () => {
     { month: 'Jul', inflow: 7000, outflow: 3000 },
     { month: 'Aug', inflow: 6500, outflow: 2200 },
   ];
-  const formatPKR = (value) => `PKR ${value.toLocaleString()}`;
+
+
+
+  // product charts data
+
+  const productTrendData = [
+
+    {
+      month: 'Jan',
+      Salteen: 9000,
+      SalteenPinkSalt: 3500,
+      SalteenPinkJar: 550,
+      SalteenJar: 9000,
+      SalteenBlackSalt: 6000,
+      SalteenRefinedTabl: 5500,
+    },
+
+    {
+      month: 'Feb',
+      Salteen: 4000,
+      SalteenPinkSalt: 3000,
+      SalteenPinkJar: 5000,
+      SalteenJar: 4500,
+      SalteenBlackSalt: 6000,
+      SalteenRefinedTabl: 5500,
+
+    },
+
+    {
+      month: 'Mar',
+      Salteen: 4000,
+      SalteenPinkSalt: 3000,
+      SalteenPinkJar: 5000,
+      SalteenJar: 4500,
+      SalteenBlackSalt: 6000,
+      SalteenRefinedTabl: 5500,
+
+    },
+    {
+      month: 'Apr',
+      Salteen: 4000,
+      SalteenPinkSalt: 3000,
+      SalteenPinkJar: 5000,
+      SalteenJar: 4500,
+      SalteenBlackSalt: 6000,
+      SalteenRefinedTabl: 5500,
+
+    },
+    {
+      month: 'May',
+      Salteen: 4000,
+      SalteenPinkSalt: 3000,
+      SalteenPinkJar: 5000,
+      SalteenJar: 4500,
+      SalteenBlackSalt: 6000,
+      SalteenRefinedTabl: 5500,
+
+    },
+    {
+      month: 'Jun',
+      Salteen: 4000,
+      SalteenPinkSalt: 3000,
+      SalteenPinkJar: 5000,
+      SalteenJar: 4500,
+      SalteenBlackSalt: 6000,
+      SalteenRefinedTabl: 5500,
+
+    },
+    {
+      month: 'Jul',
+      Salteen: 2000,
+      SalteenPinkSalt: 1000,
+      SalteenPinkJar: 500,
+      SalteenJar: 450,
+      SalteenBlackSalt: 60,
+      SalteenRefinedTabl: 550,
+    },
+    {
+      month: 'Aug',
+      Salteen: 4000,
+      SalteenPinkSalt: 3000,
+      SalteenPinkJar: 5000,
+      SalteenJar: 4500,
+      SalteenBlackSalt: 6000,
+      SalteenRefinedTabl: 5500,
+    },
+  ]
+
+
+  // top producsts
+
+const topProducts = [
+  { name: "Salteen", sales: 9000 },
+  { name: "SalteenPinkSalt", sales: 3500 },
+  { name: "SalteenPinkJar", sales: 550 },
+  { name: "SalteenJar", sales: 9000 },
+  { name: "SalteenBlackSalt", sales: 6000 },
+  { name: "SalteenRefinedTabl", sales: 5500 }
+];
 
   const ranges = [
     "Today",
@@ -49,6 +150,10 @@ const Dashboard = () => {
     "Last year",
     "All Time",
   ];
+
+
+
+
   const [selected, setSelected] = useState("Last 30 Days");
 
   const options = [];
@@ -223,50 +328,190 @@ const Dashboard = () => {
         </Row>
       </div>
 
-        {/* charts */}
 
-      <div className='bg-white my-5 rounded-md px-4 py-4'>
-        <h2 className='font-primary font-medium text-shadow-md'>Cash Flow Trend</h2>
-        <ResponsiveContainer width="100%" height={350}>
-          <AreaChart data={data} margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
-            <defs>
-              <linearGradient id="inflowGrad" x1="0" y1="0" x2="0" y2="1">
-                <stop offset="5%" stopColor="#22c55e" stopOpacity={0.4} />
-                <stop offset="95%" stopColor="#22c55e" stopOpacity={0} />
-              </linearGradient>
-              <linearGradient id="outflowGrad" x1="0" y1="0" x2="0" y2="1">
-                <stop offset="5%" stopColor="#ef4444" stopOpacity={0.4} />
-                <stop offset="95%" stopColor="#ef4444" stopOpacity={0} />
-              </linearGradient>
-            </defs>
-
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="month" />
-            <YAxis />
-            <Tooltip />
-            <Legend verticalAlign="top" align="center" />
+      {/* charts  */}
+      {/*Cash flow trend charts */}
+      <div>
 
 
-            <Area
-              type="monotone"
-              dataKey="inflow"
-              stroke="#22c55e"
-              fill="url(#inflowGrad)"
-              strokeWidth={2}
-            />
-            <Area
-              type="monotone"
-              dataKey="outflow"
-              stroke="#ef4444"
-              fill="url(#outflowGrad)"
-              strokeWidth={2}
-            />
-          </AreaChart>
-        </ResponsiveContainer>
+        <div className='bg-white my-5 rounded-md px-4 py-7'>
+          <h2 className='font-primary font-medium  text-xl'>Cash Flow Trend</h2>
+          <ResponsiveContainer width="100%" height={350}>
+            <AreaChart data={data} margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+              <defs>
+                <linearGradient id="inflowGrad" x1="0" y1="0" x2="0" y2="1">
+                  <stop offset="5%" stopColor="#22c55e" stopOpacity={0.4} />
+                  <stop offset="95%" stopColor="#22c55e" stopOpacity={0} />
+                </linearGradient>
+                <linearGradient id="outflowGrad" x1="0" y1="0" x2="0" y2="1">
+                  <stop offset="5%" stopColor="#ef4444" stopOpacity={0.4} />
+                  <stop offset="95%" stopColor="#ef4444" stopOpacity={0} />
+                </linearGradient>
+              </defs>
+
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="month" />
+              <YAxis />
+              <Tooltip />
+              <Legend verticalAlign="top" align="center" />
+
+
+              <Area
+                type="monotone"
+                dataKey="inflow"
+                stroke="#22c55e"
+                fill="url(#inflowGrad)"
+                strokeWidth={2}
+              />
+              <Area
+                type="monotone"
+                dataKey="outflow"
+                stroke="#ef4444"
+                fill="url(#outflowGrad)"
+                strokeWidth={2}
+              />
+            </AreaChart>
+          </ResponsiveContainer>
+        </div>
+
+        {/* Product sales trend charts */}
+
+        <div className='bg-white my-5 rounded-md px-4 py-7'>
+          <h2 className='font-primary font-medium  text-xl'>Product Sales Trend(Top 10 products)</h2>
+          <ResponsiveContainer width="100%" height={350}>
+            <AreaChart data={productTrendData} margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+              <defs>
+                <linearGradient id="inflowGrad" x1="0" y1="0" x2="0" y2="1">
+                  <stop offset="5%" stopColor="#22c55e" stopOpacity={0.4} />
+                  <stop offset="95%" stopColor="#22c55e" stopOpacity={0} />
+                </linearGradient>
+                <linearGradient id="outflowGrad" x1="0" y1="0" x2="0" y2="1">
+                  <stop offset="5%" stopColor="#ef4444" stopOpacity={0.4} />
+                  <stop offset="95%" stopColor="#ef4444" stopOpacity={0} />
+                </linearGradient>
+              </defs>
+
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="month" />
+              <YAxis />
+              <Tooltip />
+              <Legend verticalAlign="top" align="center" />
+
+
+              <Area
+                type="monotone"
+                dataKey="Salteen"
+                stroke="orange"
+                fill="url(#inflowGrad)"
+                strokeWidth={2}
+              />
+              <Area
+                type="monotone"
+                dataKey="SalteenPinkSalt"
+                stroke="black"
+                fill="url(#outflowGrad)"
+                strokeWidth={2}
+              />
+              <Area
+                type="monotone"
+                dataKey="SalteenPinkJar"
+                stroke="blue"
+                fill="url(#inflowGrad)"
+                strokeWidth={2}
+              />
+              <Area
+                type="monotone"
+                dataKey="SalteenJar"
+                stroke="purple"
+                fill="url(#outflowGrad)"
+                strokeWidth={2}
+              />
+              <Area
+                type="monotone"
+                dataKey="SalteenBlackSalt"
+                stroke="cyan"
+                fill="url(#outflowGrad)"
+                strokeWidth={2}
+              />
+              <Area
+                type="monotone"
+                dataKey="SalteenRefinedTabl"
+                stroke="#ef4444"
+                fill="url(#outflowGrad)"
+                strokeWidth={2}
+              />
+            </AreaChart>
+          </ResponsiveContainer>
+        </div>
       </div>
+
+
+      {/* end of charts  */}
+
+
+      <div>
+
+        {/* footer */}
+
+        <div >
+
+          <div className='flex justify-between gap-3'>
+
+            <div className='bg-white my-5 rounded-md px-4 py-7 w-[50%] h-[300px]'>
+              <div className='flex justify-between items-center mb-4'>
+
+              <h3 className=' font-primary text-lg font-bold '>Top Products</h3>
+              <button className='bg-blue-500 font-primary  px-3 py-2 rounded-md'>Manage Products</button>
+              </div>
+              {
+                topProducts.map((product)=>(
+                  <div className='flex items-center justify-between'>
+                  <p className='font-primary font-medium my-1'>{product.name}</p>
+                  <p className='font-primary font-medium text-green-500 my-1'>{product.sales}</p>
+                  </div>
+
+                ))
+              }
+              
+            </div>
+
+            <div className='bg-white my-5 rounded-md px-4 py-7 w-[50%] h-[300px]'>
+              <div className='flex justify-between items-center mb-4'>
+
+              <h3 className=' font-primary text-xl font-semibold '>Top Products</h3>
+              <button className='bg-blue-500 font-primary  px-3 py-2 rounded-md'>Manage Products</button>
+              </div>
+              {
+                topProducts.map((product)=>(
+                  <div className='flex items-center justify-between'>
+                  <p className='font-primary font-light text-lg my-1 '>{product.name}</p>
+                  <p className='font-primary font-medium text-green-500 my-1'>{product.sales}</p>
+                  </div>
+
+                ))
+              }
+              
+            </div>
+
+          </div>
+
+        </div>
+
+        {/* end of footer  */}
+
+      </div>
+
+
 
     </div>
   )
 }
+
+// Salteen
+// SalteenPinkSalt
+// SalteenPinkJar
+// SalteenJar
+// SalteenBlackSalt
+// SalteenRefinedTabl
 
 export default Dashboard
